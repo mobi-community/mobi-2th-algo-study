41417d62de2eaf56746aea62f7b19f5e8a8c2f6d
kiminn <134191815+kiminn@users.noreply.github.com>
2023-12-26 22:03:41 +0900

    feat: lv.0 풀어보기

---
+++ kimi/lv.0/1226.js
@@ -0,0 +1,43 @@
+
+
+/**
+ * 다른 사람의 풀이 보고 배우기.
+function solution(age) {
+    return new Date().getFullYear() - age + 1;
+}
+ */
+/**
+ * 머쓱이는 40살인 선생님이 몇 년도에 태어났는지 궁금해졌습니다. 나이 age가 주어질 때, 2022년을 기준 출생 연도를 return 하는 solution 함수를 완성해주세요.
+ */
+
+
+function solution(age) {
+    var answer = 2023 - age;
+    return answer;
+}
+
+
+
+/**
+ * 정수 num1, num2가 매개변수로 주어질 때, num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성해주세요.
+ * Math.floor() 함수는 주어진 숫자와 같거나 작은 정수 중에서 가장 큰 수를 반환합니다.
+ */
+
+function solution(num1, num2) {
+    const answer = Math.floor(num1 / num2);
+    return answer;
+}
+
+/**
+ * 두수의 차
+ */
+
+function solution(num1, num2) {
+    const answer = num1 - num2;
+    return answer;
+}
+
+/**
+ * 프로그래머스 및 깃허브 세팅하고.. js가 너무 오랜만이라 이것저것 알아보느라 많이하지못하였다.
+ * 내일부터 제대로..!
+ */
