d583f56a76885c339b649dc6c6628d9db79c5a66
suhyeon jung <mugi7794@naver.com>
2023-12-27 22:57:16 +0900

    feat >> 12-27 lv0 문제풀이

---
+++ Noel/lv.0/2023-12-27.js
@@ -0,0 +1,42 @@
+//정수 num1, num2가 매개변수 주어집니다. num1과 num2를 곱한 값을 return 하도록 solution 함수를 완성해주세요.
+
+function solution(num1, num2) {
+  return num1 * num2;
+}
+
+// 정수 num1과 num2가 매개변수로 주어집니다. 두 수가 같으면 1 다르면 -1을 retrun하도록 solution 함수를 완성해주세요.
+
+function solution(num1, num2) {
+  if (num1 === num2) return 1;
+  if (num1 !== num2) return -1;
+}
+
+// 머쓱이는 40살인 선생님이 몇 년도에 태어났는지 궁금해졌습니다. 나이 age가 주어질 때, 2022년을 기준 출생 연도를 return 하는 solution 함수를 완성해주세요.
+function solution(age) {
+  const birth = 2022 - age + 1;
+  return birth;
+}
+
+// 각에서 0도 초과 90도 미만은 예각, 90도는 직각, 90도 초과 180도 미만은 둔각 180도는 평각으로 분류합니다. 각 angle이 매개변수로 주어질 때 예각일 때 1, 직각일 때 2, 둔각일 때 3, 평각일 때 4를 return하도록 solution 함수를 완성해주세요.
+function solution(angle) {
+  return angle < 90 ? 1 : angle === 90 ? 2 : angle < 180 ? 3 : 4;
+}
+
+// 정수 배열 numbers가 매개변수로 주어집니다. numbers의 원소의 평균값을 return하도록 solution 함수를 완성해주세요.
+function solution(numbers) {
+  let sum = 0;
+  numbers.forEach((item) => {
+    sum += item;
+  });
+
+  var answer = sum / numbers.length;
+
+  return answer;
+}
+
+// 머쓱이는 할머니께 생신 축하 편지를 쓰려고 합니다. 할머니가 보시기 편하도록 글자 한 자 한 자를 가로 2cm 크기로 적으려고 하며, 편지를 가로로만 적을 때, 축하 문구 message를 적기 위해 필요한 편지지의 최소 가로길이를 return 하도록 solution 함수를 완성해주세요.
+
+function solution(message) {
+  var answer = message.length * 2;
+  return answer;
+}
