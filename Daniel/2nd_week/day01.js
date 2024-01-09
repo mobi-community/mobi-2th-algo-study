2ee1cbc3d041d0663bf0e5986293fca2600eab63
LeeDaeGyeong <dleorud1027@gmail.com>
2024-01-09 10:16:16 +0900

    programmers >> 치킨 쿠폰

---
+++ Daniel/2nd_week/day01.js
@@ -0,0 +1,23 @@
+/*
+    문제 2. 치킨 쿠폰
+*/
+
+function solution(chicken) {
+  let cuponCount = chicken;
+  let serviceChickenCount = 0;
+  let unusedCoupon = 0;
+
+  const firstOrder = Math.floor(cuponCount / 10);
+  serviceChickenCount += firstOrder;
+  unusedCoupon += cuponCount % 10;
+  unusedCoupon += Math.floor(firstOrder / 10);
+
+  const secondOrder = Math.floor(firstOrder / 10);
+  serviceChickenCount += secondOrder;
+  unusedCoupon += firstOrder % 10;
+  unusedCoupon += Math.floor(secondOrder / 10);
+
+  serviceChickenCount += Math.floor(unusedCoupon / 10);
+
+  return serviceChickenCount;
+}
